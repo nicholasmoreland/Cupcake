8477f0d2cb97a4358c122f328a9eabb3e2045980
nicholasmoreland <nmoreland18@outlook.com>
2022-12-14 23:06:02 -0600

    Initial commit Generated by create-expo-app 1.1.4.

--- App.js
+++ App.js
@@ -1,20 +1,17 @@
+import React from 'react';
+import { View, ImageBackground, Text } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <ImageBackground source={image} resizeMethod='cover' style={styles.image}>
+        <Text style={styles.textRegular}>
+          Welcome to, <Text style={styles.textCupcake}>Cupcake</Text>
+        </Text>
+        <StatusBar style="light" />
+      </ImageBackground>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
