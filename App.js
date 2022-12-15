2974b093009ce264df9e01844b48badecb9dddb9
nicholasmoreland <nmoreland18@outlook.com>
2022-12-14 23:53:01 -0600

    update: start of the landing screen

--- App.js
+++ App.js
@@ -1,17 +1,7 @@
-import React from 'react';
-import { View, ImageBackground, Text } from 'react-native';
-import { StatusBar } from 'expo-status-bar';
-
+import LandingScreen from './screens/LandingScreen';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <ImageBackground source={image} resizeMethod='cover' style={styles.image}>
-        <Text style={styles.textRegular}>
-          Welcome to, <Text style={styles.textCupcake}>Cupcake</Text>
-        </Text>
-        <StatusBar style="light" />
-      </ImageBackground>
-    </View>
+    <LandingScreen />
   );
 }
---
+++ screens/LandingScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { StyleSheet } from 'react-native';
+
+const LandingScreen = () => {
+
+}
+
+export default LandingScreen;
+
+const styles = StyleSheet.create({
+
+});
